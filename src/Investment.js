e2d623d677d1c19dedcc6e608d368f3bc894ea4b
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-05 16:38:35 +0400

    Investment Component

---
+++ src/Investment.js
@@ -0,0 +1,17 @@
+import React from 'react';
+class Investment extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>
+          {this.props.investment.name}
+        </td>
+        <td>
+          {this.props.investment.price}
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default Investment;
