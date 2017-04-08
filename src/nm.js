9b3d50505a44d33bcf3ed2b74e09999eb7e68500
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-08 08:34:31 +0400

    Appends currentPrice to marketprice.csv

--- src/nm.js
+++ src/nm.js
@@ -1,5 +1,9 @@
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: false});
+var fs = require('fs');
+var moment = require('moment');
+var stringify = require('csv-stringify');
+var file = '../data/marketprice.csv';
 
 getCurrentPrice = function (code, callback) {
   nightmare
@@ -15,7 +19,40 @@ getCurrentPrice = function (code, callback) {
     })
     .end()
       .then(function (result) {
-        return callback(null, result);
+        console.log(result);
+        var input = [{date: new Date(), code: code, price: result}];
+
+        var stringifier = stringify(input, {
+          formatters: {
+            date: function (value) {
+              return moment(value).format('YYYY-MM-DD');
+            }
+          }
+        }, function (err, output) {
+          console.log(output);
+          fs.appendFile(file,
+            output,
+            'utf8',
+            callback(err, output));
+        });
+        // fs.appendFile('../data/marketprice.json',
+        //   result,
+        //   callback(err, result));
+        // fs.readFile('../data/marketprice.json', function (err, data) {
+        //   if (err) {
+        //     fs.writeFile('../data/marketprice.json', JSON.stringify({date: new Date(), code: code, price: result}), function (e) {
+        //       console.log(e);
+        //     });
+        //   }
+        //   var json = JSON.parse(data);
+        //   json.push({date: new Date(), code: code, price: result});
+        //   fs.writeFile('../data/marketprice.json', JSON.stringify(json), function (writeerror) {
+        //     if (writeerror) {
+        //       throw writeerror;
+        //     }
+        //     callback(writeerror, result);
+        //   });
+        // });
       })
       .catch(function (error) {
         return callback(error, 'Error');
