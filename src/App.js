92cb2f012c9f9ede363c95b4b0958b783098f73b
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-09 08:20:36 +0400

    build static app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,58 @@
 import React, { Component } from 'react';
-import Investments from './Investments';
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// // class App extends Component {
-// //   render() {
-// //     return (
-// //       <div className="App">
-// //         <div className="App-header">
-// //           <img src={logo} className="App-logo" alt="logo" />
-// //           <h2>Welcome to React</h2>
-// //         </div>
-// //         <p className="App-intro">
-// //           To get started, edit <code>src/App.js</code> and save to reload.
-// //         </p>
-// //       </div>
-// //     );
-// //   }
-// // }
-
-var investments = [
-{category: 'Equity', price: '$49.99', stocked: true, name: 'US Technology'},
-{category: 'Income', price: '$39.99', stocked: true, name: 'PIMCO'},
-{category: 'Commodity', price: '$29.99', stocked: true, name: 'United Gold'}
-];
+import logo from './logo.svg';
+//import Investments from './Investments';
+import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      investments: [
+        {id: 1, category: 'Equity', price: '$49.99', name: 'US Technology', isComplete: true},
+        {id: 2, category: 'Income', price: '$39.99', name: 'PIMCO', isComplete: false},
+        {id: 3, category: 'Commodity', price: '$29.99', name: 'United Gold', isComplete: false}
+      ]
+    };
+  }
+
   render() {
     return (
-          <Investments investments={investments}/>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Stockify</h2>
+        </div>
+        <div className="Stockify-App">
+          <form>
+            <input type="text"/>
+          </form>
+          <div className="Stockify-List">
+            <ul>
+              {this.state.investments.map(investment =>
+                <li key={investment.id}>
+                  <input type="checkbox" defaultChecked={investment.isComplete}/>{investment.name}
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
     );
   }
 }
+
+// var investments = [
+// {category: 'Equity', price: '$49.99', stocked: true, name: 'US Technology'},
+// {category: 'Income', price: '$39.99', stocked: true, name: 'PIMCO'},
+// {category: 'Commodity', price: '$29.99', stocked: true, name: 'United Gold'}
+// ];
+
+// class App extends Component {
+//   render() {
+//     return (
+//           <Investments investments={investments}/>
+//     );
+//   }
+// }
 export default App;
 
