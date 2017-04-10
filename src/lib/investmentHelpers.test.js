fe0b6eb938512116e360e01ad3b9dc3a051c5389
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-10 20:02:27 +0400

    adds test cases

---
+++ src/lib/investmentHelpers.test.js
@@ -0,0 +1,39 @@
+import { addInvestment } from './investmentHelpers';
+
+test('addInvestment should add the passed investment to the list', () => {
+  const startInvesments = [
+        {id: 1, category: 'Equity', price: 49.99, name: 'US Technology', isComplete: true},
+        {id: 2, category: 'Income', price: 39.99, name: 'PIMCO', isComplete: false}
+  ];
+
+  const newInvestment = {id: 3, category: 'Commodity', price: 29.99, name: 'United Gold', isComplete: false};
+
+  const expected = [
+        {id: 1, category: 'Equity', price: 49.99, name: 'US Technology', isComplete: true},
+        {id: 2, category: 'Income', price: 39.99, name: 'PIMCO', isComplete: false},
+        {id: 3, category: 'Commodity', price: 29.99, name: 'United Gold', isComplete: false}
+  ];
+
+  const result = addInvestment(startInvesments, newInvestment);
+
+  expect(result).toEqual(expected);
+});
+
+test('addInvestment should not mutate the existing investments array', () => {
+  const startInvesments = [
+        {id: 1, category: 'Equity', price: 49.99, name: 'US Technology', isComplete: true},
+        {id: 2, category: 'Income', price: 39.99, name: 'PIMCO', isComplete: false}
+  ];
+
+  const newInvestment = {id: 3, category: 'Commodity', price: 29.99, name: 'United Gold', isComplete: false};
+
+  const expected = [
+        {id: 1, category: 'Equity', price: 49.99, name: 'US Technology', isComplete: true},
+        {id: 2, category: 'Income', price: 39.99, name: 'PIMCO', isComplete: false},
+        {id: 3, category: 'Commodity', price: 29.99, name: 'United Gold', isComplete: false}
+  ];
+
+  const result = addInvestment(startInvesments, newInvestment);
+
+  expect(result).not.toBe(startInvesments);
+});
