ea4f8793d7c9df0ca23071821f8ce2e4d4c34392
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-10 20:01:28 +0400

    Adds Investment Form component

--- src/components/investment/InvestmentForm.js
+++ src/components/investment/InvestmentForm.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export const InvestmentForm = (props) => (
-            <form>
+            <form onSubmit={props.handleSubmit}>
               <input type="text"
                      onChange={props.handleInputChange}
                      value={props.currentInvestment}/>
             </form>
           );
+
+InvestmentForm.propTypes = {
+  currentInvestment: React.PropTypes.string.isRequired,
+  handleInputChange: React.PropTypes.func.isRequired,
+  handleSubmit: React.PropTypes.func.isRequired
+};
