52c6e3bff414cc0b03e3c8431191ee7a92ad1a63
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-13 12:29:37 +0400

    adds name attributes to the input elements and submit button

--- src/components/investment/InvestmentForm.js
+++ src/components/investment/InvestmentForm.js
@@ -2,15 +2,26 @@ import React from 'react';
 
 export const InvestmentForm = (props) => (
             <form onSubmit={props.handleSubmit}>
-              <input type="text"
-                     name="somethingname"
-                     onChange={props.handleInputChange}
-                     value={props.currentInvestmentName}/>
+              <div>
+                <input type="text"
+                       name="investmentName"
+                       onChange={props.handleInputChange}
+                       value={props.investmentName}/>
+              </div>
+              <div>
+                <input type="text"
+                       name="category"
+                       onChange={props.handleInputChange}
+                       value={props.category}/>
+              </div>
+              <div>
+                <input type="submit" value="Submit" />
+              </div>
             </form>
           );
 
-InvestmentForm.propTypes = {
-  currentInvestmentName: React.PropTypes.string.isRequired,
-  handleInputChange: React.PropTypes.func.isRequired,
-  handleSubmit: React.PropTypes.func.isRequired
-};
+// InvestmentForm.propTypes = {
+//   currentInvestmentName: React.PropTypes.string.isRequired,
+//   handleInputChange: React.PropTypes.func.isRequired,
+//   handleSubmit: React.PropTypes.func.isRequired
+// };
