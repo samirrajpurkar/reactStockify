2842da4e03fcdddaffbf538bcde7da1e4fb45f5a
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-17 22:08:58 +0400

    adds finds investment by ID helper

--- src/lib/investmentHelpers.js
+++ src/lib/investmentHelpers.js
@@ -1,3 +1,4 @@
 export const addInvestment = (investments, newinvestment) => [...investments, newinvestment];
 
+export const findById = (id, list) => list.find(item => item.id === id);
 export const generateId = () => Math.floor(Math.random() * 100000);
