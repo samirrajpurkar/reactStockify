e8e635d4205a3495526cba3f363180e5f9043c94
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-17 22:09:15 +0400

    test findById

--- src/lib/investmentHelpers.test.js
+++ src/lib/investmentHelpers.test.js
@@ -1,4 +1,15 @@
-import { addInvestment } from './investmentHelpers';
+import { addInvestment, findById } from './investmentHelpers';
+
+test('findById should return the expected item from the array', () => {
+  const startInvesments = [
+        {id: 1, category: 'Equity', price: 49.99, name: 'US Technology', isComplete: true},
+        {id: 2, category: 'Income', price: 39.99, name: 'PIMCO', isComplete: false},
+        {id: 3, category: 'Commodity', price: 29.99, name: 'United Gold', isComplete: false}
+  ];
+  const expected = {id: 2, category: 'Income', price: 39.99, name: 'PIMCO', isComplete: false};
+  const result = findById(2, startInvesments);
+  expect(result).toEqual(expected);
+});
 
 test('addInvestment should add the passed investment to the list', () => {
   const startInvesments = [
