5231d08e7694d398a5e1bdbe16d7bb56cfb9307c
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-18 20:57:57 +0400

    toggleInvestment helper function

--- src/lib/investmentHelpers.js
+++ src/lib/investmentHelpers.js
@@ -1,4 +1,5 @@
 export const addInvestment = (investments, newinvestment) => [...investments, newinvestment];
 
+export const toggleInvestment = (investment) => ({...investment, isComplete: !investment.isComplete});
 export const findById = (id, list) => list.find(item => item.id === id);
 export const generateId = () => Math.floor(Math.random() * 100000);
