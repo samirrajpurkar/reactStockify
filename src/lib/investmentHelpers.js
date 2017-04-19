e63307948f04497f723af4b5aadf2a224907961a
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-19 20:17:27 +0400

    updateInvestment Helper Function

--- src/lib/investmentHelpers.js
+++ src/lib/investmentHelpers.js
@@ -1,5 +1,13 @@
 export const addInvestment = (investments, newinvestment) => [...investments, newinvestment];
 
+export const updateInvestment = (list, investment) => {
+  const index = list.findIndex((item) => item.id === investment.id);
+  return [
+    ...list.slice(0, index),
+    investment,
+    ...list.slice(index + 1)
+  ];
+};
 export const toggleInvestment = (investment) => ({...investment, isComplete: !investment.isComplete});
 export const findById = (id, list) => list.find(item => item.id === id);
 export const generateId = () => Math.floor(Math.random() * 100000);
