131a60c3cdc7ceb4ada35e168da5c8a93afa5d2e
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-04-30 19:33:34 +0400

    Consume Route context in the Link Component to manage browser history

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 
 export class Link extends Component {
+  static contextTypes = {
+    route: React.PropTypes.string,
+    linkHandler: React.PropTypes.func
+  }
+
   handleClick = (evt) => {
     evt.preventDefault();
-    history.pushState(null,'',this.props.to); //history.pushState(state, title, location in the browser)
+    //history.pushState(null,'',this.props.to); //history.pushState(state, title, location in the browser)
+    this.context.linkHandler(this.props.to);
   }
 
   render() {
-    return <a href="#" onClick={this.handleClick}>{this.props.children}</a>;
+    const activeRouteClass = this.context.route === this.props.to ? 'activeRoute' : '';
+    return <a className={activeRouteClass} href="#" onClick={this.handleClick}>{this.props.children}</a>;
   }
 }
 
