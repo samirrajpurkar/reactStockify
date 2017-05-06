bc99783b62d03b68ea743b0473174e01e1eea5d8
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-06 18:32:30 +0400

    createInvestment utility function

--- src/lib/investmentService.js
+++ src/lib/investmentService.js
@@ -4,3 +4,15 @@ export const loadInvestments = () => {
   return fetch(baseurl)
     .then(res => res.json());
 };
+
+export const createInvestment = (investment) => {
+  return fetch(baseurl, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(investment)
+  })
+    .then(res => res.json());
+};
