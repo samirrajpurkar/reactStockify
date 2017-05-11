527253fa16396b516d1d7ca6251a33a32c43868e
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-11 16:33:13 +0400

    exposes web services at port 5555

--- src/lib/priceService.js
+++ src/lib/priceService.js
@@ -1,8 +1,13 @@
-var Xray = require('x-ray');
-var x = Xray();
+var xray = require('x-ray')();
+var app = require('express')();
 
-export const getMarketPrice = () => {
-  x('https://www.bloomberg.com/quote/NKY:IND', '.price')(function (error, price) {
-    console.log(price);
-  });
-};
+// export const getMarketPrice = () => {
+//   xray('https://www.bloomberg.com/quote/NKY:IND', '.price')(function (error, price) {
+//     console.log(price);
+//   });
+// };
+app.get('/', function (req, res) {
+  var stream = xray('https://www.bloomberg.com/quote/NKY:IND', '.price').stream();
+  stream.pipe(res);
+});
+app.listen(5555);
