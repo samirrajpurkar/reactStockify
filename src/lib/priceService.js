fe63fa2efb063a93ead5ada8a843d6d49678056f
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-12 13:37:57 +0400

    returns a object with date and price

--- src/lib/priceService.js
+++ src/lib/priceService.js
@@ -1,13 +1,11 @@
 var xray = require('x-ray')();
 var app = require('express')();
 
-// export const getMarketPrice = () => {
-//   xray('https://www.bloomberg.com/quote/NKY:IND', '.price')(function (error, price) {
-//     console.log(price);
-//   });
-// };
 app.get('/', function (req, res) {
-  var stream = xray('https://www.bloomberg.com/quote/NKY:IND', '.price').stream();
+  var stream = xray('https://www.bloomberg.com/quote/NKY:IND','.basic-quote', {
+    date: '.price-datetime',
+    price: '.price'
+  }).stream();
   stream.pipe(res);
 });
 app.listen(5555);
