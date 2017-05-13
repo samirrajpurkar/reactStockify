29f10650a0fadbb59323fd121817430abdcec3cf
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-13 12:39:41 +0400

    change to React Component

--- src/components/investment/Investment.js
+++ src/components/investment/Investment.js
@@ -1,34 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Investment.css';
 import {partial} from '../../lib/utils';
+import {getPriceFromBloomberg} from '../../lib/price';
 
-export const Investment = (props) => {
-  //const handleToggle = () => props.handleToggle(props.id);
-  //const handleToggle = props.handleToggle.bind(null, props.id);
-  const handleToggle = partial(props.handleToggle, props.id);
-  const handleRemove = partial(props.handleRemove, props.id);
+export class Investment extends Component {
+  handleToggle = partial(this.props.handleToggle, this.props.id);
+  handleRemove = partial(this.props.handleRemove, this.props.id);
 
-  return (
-    <div className="Investment">
-      <li>
-        <span className="removeInvestment">
-          <a href="#" onClick={handleRemove}>X</a>
-        </span>
-        <span className="main">
-          <input type="checkbox"
-            onChange={handleToggle}
-            checked={props.isComplete}/>{props.name}
-        </span>
-        <span className="others">
-          {props.price}
-        </span>
-        <span className="others">
-          {props.category}
-        </span>
-      </li>
-    </div>
-  );
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      marketprice: props.current_price
+    };
+  }
+
+  removeComma = (s) => (s.replace(',',''));
+
+  componentDidMount() {
+    getPriceFromBloomberg(this.props.investment_code)
+      .then(result => {
+        this.setState({marketprice: this.removeComma(result.price)});
+      });
+  }
+
+  render() {
+    return (
+      <div className="Investment">
+        <li>
+          <span className="removeInvestment">
+            <a href="#" onClick={this.handleRemove}>X</a>
+          </span>
+          <span className="main">
+            <input type="checkbox"
+              onChange={this.handleToggle}
+              checked={this.props.isComplete}/>{this.props.name}
+          </span>
+          <span className="others">
+            {this.props.price}
+          </span>
+          <span className="others">
+            {this.props.category}
+          </span>
+          <span className="others">
+            {this.state.marketprice}
+          </span>
+          <span className="others">
+            {parseFloat(this.props.price) - parseFloat(this.state.marketprice)}
+          </span>
+        </li>
+      </div>
+    );
+  }
+}
 
 Investment.propTypes = {
   name: React.PropTypes.string.isRequired,
