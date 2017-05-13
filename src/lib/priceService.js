f55fa9ce92e3c00958f9974eebfbf8b347f62066
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-13 12:40:11 +0400

    price server

--- src/lib/priceService.js
+++ src/lib/priceService.js
@@ -2,6 +2,17 @@ var xray = require('x-ray')();
 var app = require('express')();
 
 app.get('/', function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+  });
+
   var stream = xray('https://www.bloomberg.com/quote/NKY:IND','.basic-quote', {
     date: '.price-datetime',
     price: '.price'
@@ -9,3 +20,4 @@ app.get('/', function (req, res) {
   stream.pipe(res);
 });
 app.listen(5555);
+
