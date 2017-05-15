3498021a2a086c9bb8765623a644a0b28d089482
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-15 20:20:24 +0400

    style InvestmentForm

--- src/components/investment/InvestmentForm.js
+++ src/components/investment/InvestmentForm.js
@@ -3,13 +3,8 @@ import './InvestmentForm.css';
 
 export const InvestmentForm = (props) => (
             <form onSubmit={props.handleSubmit}>
-              <div>
-                <label>
-                  Category
-                </label>
-                  <select
-                         name="category"
-                         onChange={props.handleInputChange}>
+              <div className="form-group row">
+                  <select className="form-control" id="category" name="category" onChange={props.handleInputChange}>
                     <option value="Equity">Equity</option>
                     <option value="Bond">Bond</option>
                     <option value="Commodities">Commodities</option>
@@ -17,36 +12,45 @@ export const InvestmentForm = (props) => (
               </div>
 
               <div>
-                <label>
-                  Price
-                </label>
-                  <input type="text"
-                         name="price"
-                         onChange={props.handleInputChange}
-                         value={props.price}/>
+                <div className="form-group row">
+                    <input type="number"
+                           name="price"
+                           onChange={props.handleInputChange}
+                           value={props.price}
+                           className="form-control"
+                           id="price"
+                           placeholder="Cost Price example 100"
+                           />
+                </div>
               </div>
               <div>
-                <label>
-                  Investnemt Name
-                </label>
-                  <input type="text"
-                         name="investmentName"
-                         onChange={props.handleInputChange}
-                         value={props.investmentName}/>
-              </div>
-
-              <div>
-                <label>
-                  Investnemt Code
-                </label>
-                  <input type="text"
-                         name="invesment_code"
-                         onChange={props.handleInputChange}
-                         value={props.invesment_code}/>
+                <div className="form-group row">
+                    <input type="text"
+                           name="investmentName"
+                           onChange={props.handleInputChange}
+                           value={props.investmentName}
+                           className="form-control"
+                           id="investmentName"
+                           placeholder="Invesment Name"
+                           />
+                </div>
               </div>
 
               <div>
-                <input type="submit" value="Submit" className="button" />
+                <div className="form-group row">
+                    <input type="text"
+                           name="invesment_code"
+                           onChange={props.handleInputChange}
+                           value={props.invesment_code}
+                           className="form-control"
+                           id="invesment_code"
+                           placeholder="Investment Code"
+                           />
+                </div>
+              </div>
+
+              <div>
+                <input type="submit" value="Submit" className="btn btn-secondary btn-sm" />
               </div>
             </form>
           );
