ea4ad80deb7ced82ae2e90dec3e626501c877458
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-18 22:35:09 +0400

    removeComma added as a utility function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,3 +2,10 @@ export const partial = (fn, ...args) => fn.bind(null, ...args);
 
 const _pipe = (f, g) => (...args) => g(f(...args));
 export const pipe = (...fns) => fns.reduce(_pipe);
+export const removeComma = (s) => {
+  if (s && s.indexOf(',') === -1) {
+    return s;
+  } else {
+    return s.replace(',','');
+  }
+};
