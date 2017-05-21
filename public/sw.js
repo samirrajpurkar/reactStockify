2d1aad75071d72d81d27ef04aeda64ff5c09d23e
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-21 19:45:37 +0400

    removes buggy service worker code

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-// When the webpage goes to fetch files, we intercept that request and serve up the matching files
-// if we have them
-self.addEventListener('fetch', function (event) {
-  console.log('Hello');
-});
