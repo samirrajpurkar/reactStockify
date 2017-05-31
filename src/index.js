ecb69dbbb4bf3624570d133e4831c2a10b93787a
samirrajpurkar <samir.rajpurkar@gmail.com>
2017-05-31 19:38:08 +0400

    Reverting router related changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import NavBar from './components/investment/NavBar';
+import App from './App';
+//import NavBar from './components/investment/NavBar';
 
 //import './index.css';
 //import {Router} from './components/router';
 
 ReactDOM.render(
     (
-      <NavBar />
+      <App/>
     ),
   document.getElementById('container')
 );
